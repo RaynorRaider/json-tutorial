7a85137a9ae7ffc910d8161331d5d6c77905425c
李骏 <573426433@qq.com>
2016-09-25 16:45:03 +0800

    Update leptjson.c

--- tutorial01/leptjson.c
+++ tutorial01/leptjson.c
@@ -64,6 +64,7 @@ int lept_parse(lept_value* v, const char* json) {
 		lept_parse_whitespace(&c);
 		if (*c.json != '\0')
 		{
+			v->type = LEPT_NULL;
 			ret = LEPT_PARSE_ROOT_NOT_SINGULAR;
 		}
 	}
